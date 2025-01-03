5e8eb4a68776c65ee8f4a02c9135e002254b7c51
taojiu <mazhaobo@hoteamsoft.com>
2025-01-03 16:19:47 +0800

    build: 删除exports中default和style.css配置

--- packages/types/src/shell/type/registered-setter.ts
+++ packages/types/src/shell/type/registered-setter.ts
@@ -1,5 +1,5 @@
 import { IPublicModelSettingField } from '../model';
-import { IPublicTypeCustomView, IPublicTypeTitleContent } from './';
+import { IPublicTypeCompositeValue, IPublicTypeCustomView, IPublicTypeTitleContent } from './';
 
 export interface IPublicTypeRegisteredSetter {
   component: IPublicTypeCustomView;
@@ -10,6 +10,12 @@ export interface IPublicTypeRegisteredSetter {
    * for MixedSetter to check this setter if available
    */
   condition?: (field: IPublicModelSettingField) => boolean;
+  /**
+   * 给 MixedSetter，切换值时声明类型
+   *
+   * @todo 物料协议推进
+   */
+  valueType?: IPublicTypeCompositeValue[];
 
   /**
    * for MixedSetter to manual change to this setter
